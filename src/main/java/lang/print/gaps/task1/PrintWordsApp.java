6882a73bf5cfd85d1920441b1372e209bbaadb58
Talha Keles <kelesstalha@gmail.com>
2022-09-24 00:42:10 +0300

    Everything is done

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Java\nis\ngood");
+        System.out.println("java\n"+"is\n"+"smth");
     }
 }
